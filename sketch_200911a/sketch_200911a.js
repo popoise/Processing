08198b580f9394d814d2c8fc949f361f408e3b97
10501 공태윤 <70361271+popoise@users.noreply.github.com>
2020-09-11 14:46:24 +0900

    2020-9/11 assignment

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500);
+  fill(255, 0, 255);
+  ellipse(300, 300, 250, 270);
+  fill(20, 110, 20);
+  ellipse(300, 150, 40, 120);
+  fill(100, 100, 100);
+  ellipse(100, 100, 30, 40);
+  fill(0, 0, 255);
+  ellipse(400, 100, 30, 40);
+  fill(255, 255, 255);
+  ellipse(300, 400, 40, 40);
+}
+
+
+function draw() {
+
+}
