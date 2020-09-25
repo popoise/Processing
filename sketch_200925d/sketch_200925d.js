8fba61768f9b87b455491cb65ebd352c1f65dafa
10501 공태윤 <70361271+popoise@users.noreply.github.com>
2020-09-25 19:13:50 +0900

    may not be 100% correct

---
+++ sketch_200925d/sketch_200925d.js
@@ -0,0 +1,19 @@
+let size = 10;
+
+function setup() {
+  createCanvas(500, 500);
+  noFill();
+
+
+  for(let i=0; i<40; i+=10){// repeating 4 times
+    for(let j=0; j<100; j+=20){ // repeating 5 times -> total 20 times
+     let size = (random(1, 300));
+      rect(i, j, size, size); 
+    }           
+  }
+}
+
+
+function draw() {
+
+}
